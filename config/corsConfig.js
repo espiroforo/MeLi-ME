0b3104f86dad781ffce994ab2560af961c1bf48e
rampante <gonzalojrey@gmail.com>
2022-06-24 17:45:56 -0300

    opciones de CORS cambiadas

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -1,5 +1,6 @@
 const corsOptions = {
     origin: ["http://localhost.com", "https://localhost.com", "*"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
